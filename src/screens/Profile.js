3990de76604c65e69f2dc499ee15be0d2f46d156
chandra655 <chandra.penugonda@unacademy.com>
2020-10-18 17:21:13 +0530

    fetch data

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 
+const URL = `https://api.github.com/users`;
+
 export default function Profile() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await (await fetch(URL)).json();
+      setData(res);
+    };
+    getData();
+  }, []);
+  console.log(data);
   return (
     <View>
       <Text>Profile</Text>
